462484d9ff013645a3b262f89d557627eb2ec16e
Ivan Lopez <fuchi@live.com>
2019-03-19 15:58:34 -0600

    segundo programa entrada
    programa para entradas de datos por teclado

---
+++ entrada.py
@@ -0,0 +1,6 @@
+#CAPTURAMOS LA ENTRADA DE TECLADO Y LA GUARDAMOS EN UNA VARIABLE
+nombre = input("Ingresar Nombre: ")
+apellido = input("Ingresar su Apellido")
+edad = input("Ingresar su edad")
+
+#Imprimimos lo capturado por teclado
